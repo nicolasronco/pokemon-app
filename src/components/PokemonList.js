1b769c1a9f08c00a5c718a0010ca975dd559d10b
RONCO Nicolas <NRO2@dsone.3ds.com>
2022-10-24 16:52:06 +0200

    feat: search by pokemon types

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -6,10 +6,23 @@ function PokemonList({ pokemons, inputValue }) {
 
   useEffect(() => {}, [inputValue]);
 
+  const searchPokemon = (pokemon) => {
+    let isFound = false;
+
+    if (pokemon.name.toLowerCase().includes(inputValue.toLowerCase())) {
+      isFound = true;
+    } else {
+      const types = pokemon.types.map(type => type.type.name);
+      isFound = types.find(type => type.includes(inputValue));
+    }
+
+    return isFound;
+  };
+
   return (
     <div className="pokemon-list">
       {pokemons
-        .filter(pokemon => pokemon.name.toLowerCase().includes(inputValue.toLowerCase()))
+        .filter(pokemon => searchPokemon(pokemon))
         .map(pokemon => <PokemonTile key={pokemon.id} pokemon={pokemon}/>)
       }
     </div>
